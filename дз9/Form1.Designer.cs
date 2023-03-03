bff40103e5dae93fadeb77a5b64bbbf39c583ed3
Bohdanna Kalynchuk <dana398076@gmail.com>
2023-03-03 21:58:49 +0200

    2

---
+++ дз9/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace дз9
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("Control", 0);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.detailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.largeIconToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.smallIconToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dyrectoryInfoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileInfoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 58);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(305, 380);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(807, 58);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(363, 380);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.detailsToolStripMenuItem,
+            this.largeIconToolStripMenuItem,
+            this.listToolStripMenuItem,
+            this.smallIconToolStripMenuItem,
+            this.tileToolStripMenuItem,
+            this.dyrectoryInfoToolStripMenuItem,
+            this.fileInfoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1182, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // detailsToolStripMenuItem
+            // 
+            this.detailsToolStripMenuItem.Name = "detailsToolStripMenuItem";
+            this.detailsToolStripMenuItem.Size = new System.Drawing.Size(69, 24);
+            this.detailsToolStripMenuItem.Text = "Details";
+            this.detailsToolStripMenuItem.Click += new System.EventHandler(this.detailsToolStripMenuItem_Click_1);
+            // 
+            // largeIconToolStripMenuItem
+            // 
+            this.largeIconToolStripMenuItem.Name = "largeIconToolStripMenuItem";
+            this.largeIconToolStripMenuItem.Size = new System.Drawing.Size(88, 24);
+            this.largeIconToolStripMenuItem.Text = "LargeIcon";
+            this.largeIconToolStripMenuItem.Click += new System.EventHandler(this.largeIconToolStripMenuItem_Click_1);
+            // 
+            // listToolStripMenuItem
+            // 
+            this.listToolStripMenuItem.Name = "listToolStripMenuItem";
+            this.listToolStripMenuItem.Size = new System.Drawing.Size(45, 24);
+            this.listToolStripMenuItem.Text = "List";
+            this.listToolStripMenuItem.Click += new System.EventHandler(this.listToolStripMenuItem_Click_1);
+            // 
+            // smallIconToolStripMenuItem
+            // 
+            this.smallIconToolStripMenuItem.Name = "smallIconToolStripMenuItem";
+            this.smallIconToolStripMenuItem.Size = new System.Drawing.Size(88, 24);
+            this.smallIconToolStripMenuItem.Text = "SmallIcon";
+            this.smallIconToolStripMenuItem.Click += new System.EventHandler(this.smallIconToolStripMenuItem_Click_1);
+            // 
+            // tileToolStripMenuItem
+            // 
+            this.tileToolStripMenuItem.Name = "tileToolStripMenuItem";
+            this.tileToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
+            this.tileToolStripMenuItem.Text = "Tile";
+            this.tileToolStripMenuItem.Click += new System.EventHandler(this.tileToolStripMenuItem_Click_1);
+            // 
+            // dyrectoryInfoToolStripMenuItem
+            // 
+            this.dyrectoryInfoToolStripMenuItem.Name = "dyrectoryInfoToolStripMenuItem";
+            this.dyrectoryInfoToolStripMenuItem.Size = new System.Drawing.Size(117, 24);
+            this.dyrectoryInfoToolStripMenuItem.Text = "Dyrectory info";
+            this.dyrectoryInfoToolStripMenuItem.Click += new System.EventHandler(this.dyrectoryInfoToolStripMenuItem_Click);
+            // 
+            // fileInfoToolStripMenuItem
+            // 
+            this.fileInfoToolStripMenuItem.Name = "fileInfoToolStripMenuItem";
+            this.fileInfoToolStripMenuItem.Size = new System.Drawing.Size(76, 24);
+            this.fileInfoToolStripMenuItem.Text = "File info";
+            this.fileInfoToolStripMenuItem.Click += new System.EventHandler(this.fileInfoToolStripMenuItem_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Type";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Date create";
+            this.columnHeader3.Width = 120;
+            // 
+            // listView1
+            // 
+            this.listView1.AllowDrop = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(344, 58);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(443, 380);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.ItemDrag += new System.Windows.Forms.ItemDragEventHandler(this.listView1_ItemDrag);
+            this.listView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDown);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "завантаження.png");
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem detailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem largeIconToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem smallIconToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tileToolStripMenuItem;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ToolStripMenuItem dyrectoryInfoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileInfoToolStripMenuItem;
+    }
+}
+
---
+++ дз9/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace дз9
+{
+    public partial class Form1 : Form
+    {
+        ImageList Large = new ImageList();
+        ImageList Smole = new ImageList();
+        ImageList Detail = new ImageList();
+        public Form1()
+        {
+            InitializeComponent();
+            GetFolder();
+            listView1.AllowDrop = true;
+            listView1.Items.Clear();
+            listView1.View = View.List;
+            richTextBox1.AllowDrop = true;
+            richTextBox1.DragDrop += RichTextBox1_DragDrop;
+            richTextBox1.DragEnter += RichTextBox1_DragEnter;
+
+        }
+
+        private void GetFolder()
+        {
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                var node = new TreeNode(drive.Name);
+                node.Tag = drive.RootDirectory;
+                node.Nodes.Add("");
+                treeView1.Nodes.Add(node);
+            }
+        }
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            var currentNode = e.Node;
+            if (currentNode.Nodes.Count == 1 && currentNode.Nodes[0].Text == "")
+            {
+
+                currentNode.Nodes.RemoveAt(0);
+                try
+                {
+                    var directories = Directory.GetDirectories(currentNode.Tag.ToString());
+                    //textBox2.Text = currentNode.Tag.ToString();
+                    foreach (var directory in directories)
+                    {
+                        var node = new TreeNode(Path.GetFileName(directory));
+                        node.Tag = directory;
+                        node.Nodes.Add("");
+                        currentNode.Nodes.Add(node);
+                    }
+                }
+                catch (UnauthorizedAccessException)
+                {
+
+                }
+            }
+        }
+
+        private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+
+            if (Directory.Exists(e.Node.Tag.ToString()))
+            {
+
+                listView1.Items.Clear();
+                DirectoryInfo directory = new DirectoryInfo(e.Node.Tag.ToString());
+                FileInfo[] info = directory.GetFiles();
+                foreach (var file in info)
+                {
+                    ListViewItem list = new ListViewItem(file.Name,0);
+                    list.SubItems.Add(file.Extension.ToString());
+                    list.SubItems.Add(file.CreationTime.ToShortDateString());
+                    list.Tag = file.FullName;
+                    listView1.Items.Add(list);
+                    
+                    
+                }
+            }
+        }
+
+        private void detailsToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Detail.ImageSize = new Size(15, 15);
+            Detail.Images.Add(imageList1.Images[0]);
+            listView1.View = View.Details;
+            
+
+        }
+
+        private void largeIconToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Large.ImageSize = new Size(150, 150);
+            Large.Images.Add(imageList1.Images[0]);
+            listView1.LargeImageList = Large;
+            listView1.View = View.LargeIcon;
+        }
+
+        private void listToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            listView1.View = View.List;
+            
+        }
+
+        private void smallIconToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Smole.ImageSize = new Size(50, 50);
+            Smole.Images.Add(imageList1.Images[0]);
+            listView1.SmallImageList = Smole;
+            listView1.View = View.SmallIcon;
+            
+        }
+        
+        private void tileToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            listView1.View = View.Tile;
+        }
+        //Код для перетягування файлу
+
+        private void listView1_ItemDrag(object sender, ItemDragEventArgs e)
+        {
+
+            ListView.SelectedIndexCollection indexes =
+                this.listView1.SelectedIndices;
+
+            foreach (int item in indexes)
+            {
+                string path = this.listView1.Items[item].Tag.ToString();
+                listView1.DoDragDrop(path, DragDropEffects.Copy);
+            }
+        }
+
+        private void RichTextBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void RichTextBox1_DragDrop(object sender, DragEventArgs e)
+        {
+
+            string files = (string)e.Data.GetData(DataFormats.Text);
+                try
+                {
+                    StreamReader reader = File.OpenText(files);
+                    richTextBox1.Text = String.Empty;
+                    richTextBox1.Text = reader.ReadToEnd();
+
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Помилка: {ex.Message}", "Помилка");
+                }
+
+        }
+        private void listView1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ListView.SelectedIndexCollection indexes =
+                this.listView1.SelectedIndices;
+
+            foreach (int item in indexes)
+            {
+                string path = this.listView1.Items[item].Tag.ToString();
+                listView1.DoDragDrop(path, DragDropEffects.Copy);
+            }
+        }
+        //код для отримання інформації про папку чи файл
+
+        private void dyrectoryInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (treeView1.SelectedNode == null)
+            {
+                MessageBox.Show("select the Directory");
+                return;
+            }
+            if (Directory.Exists(treeView1.SelectedNode.Tag.ToString()))
+            {
+
+                DirectoryInfo directory = new DirectoryInfo(treeView1.SelectedNode.Tag.ToString());
+               
+                MessageBox.Show($"{directory.FullName},\n{directory.Name},\n{directory.CreationTime},\n{directory.Attributes},\n{directory.LastWriteTime}" +
+                                $"\n{directory.Parent} ,\n{ GetDirectorySize(directory)}");
+            }
+
+        }
+         
+       long GetDirectorySize(DirectoryInfo directory)
+            {
+            long size = 0;
+            try
+            {
+                FileInfo[] files = directory.GetFiles();
+                DirectoryInfo[] directories = directory.GetDirectories();
+                foreach (FileInfo file in files)
+                {
+                    size += file.Length;
+                }
+
+                foreach (DirectoryInfo subDirectory in directories)
+                {
+                    size += GetDirectorySize(subDirectory);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка: {ex.Message}", "Помилка");
+            }
+            return size;
+       }
+        
+
+       private void fileInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ListView.SelectedIndexCollection indexes =
+               this.listView1.SelectedIndices;
+            
+            foreach (int item in indexes)
+            {    
+                string path = this.listView1.Items[item].Tag.ToString();
+                FileInfo info = new FileInfo(path);
+              
+               MessageBox.Show($"{info.FullName},\n{info.Name},\n{info.CreationTime},\n{info.Extension},\n{info.LastWriteTime}");
+            }
+            
+             
+        }
+
+       
+    }
+    
+}
